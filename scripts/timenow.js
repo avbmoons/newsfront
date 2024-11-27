4a04826812895672e90bfa7d493e361d94551d34
avbmoons <avb.moons@gmail.com>
2024-11-28 00:02:54 +0300

    add admin layout

---
+++ scripts/timenow.js
@@ -0,0 +1,6 @@
+var timeElement = document.getElementById('currentTime');
+setInterval(function () {
+    var currentTime = new Date();
+    //timeElement.textContent = currentTime.toLocaleTimeString();
+    timeElement.textContent = currentTime.toLocaleString();
+}, 1000);
