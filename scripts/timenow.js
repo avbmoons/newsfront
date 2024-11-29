7d0d9bd08552b766d214fe94b5b56f01a350ae6b
Elena-admin <avb.moons@gmail.com>
2024-11-29 20:45:00 +0300

    add components

--- scripts/timenow.js
+++ scripts/timenow.js
@@ -11,3 +11,17 @@ setInterval(function () {
   //timeElement.textContent = currentTime.toLocaleTimeString();
   timeElement.textContent = currentTime.toLocaleString();
 }, 1000);
+
+var timeElement = document.getElementById('currentTime2');
+setInterval(function () {
+  var currentTime = new Date();
+  //timeElement.textContent = currentTime.toLocaleTimeString();
+  timeElement.textContent = currentTime.toLocaleString();
+}, 1000);
+
+var timeElement = document.getElementById('currentTime3');
+setInterval(function () {
+  var currentTime = new Date();
+  //timeElement.textContent = currentTime.toLocaleTimeString();
+  timeElement.textContent = currentTime.toLocaleString();
+}, 1000);
