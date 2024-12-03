671536df8c85e04979fe9cc289e1b5560bbfec7c
avbmoons <avb.moons@gmail.com>
2024-12-03 22:53:54 +0300

    add pages layouts

--- scripts/timenow.js
+++ scripts/timenow.js
@@ -1,27 +1,8 @@
-var timeElement = document.getElementById('currentTime');
+var timeElement = document.getElementById('currentTime*');
 setInterval(function () {
   var currentTime = new Date();
   //timeElement.textContent = currentTime.toLocaleTimeString();
   timeElement.textContent = currentTime.toLocaleString();
 }, 1000);
 
-var timeElement = document.getElementById('currentTime1');
-setInterval(function () {
-  var currentTime = new Date();
-  //timeElement.textContent = currentTime.toLocaleTimeString();
-  timeElement.textContent = currentTime.toLocaleString();
-}, 1000);
 
-var timeElement = document.getElementById('currentTime2');
-setInterval(function () {
-  var currentTime = new Date();
-  //timeElement.textContent = currentTime.toLocaleTimeString();
-  timeElement.textContent = currentTime.toLocaleString();
-}, 1000);
-
-var timeElement = document.getElementById('currentTime3');
-setInterval(function () {
-  var currentTime = new Date();
-  //timeElement.textContent = currentTime.toLocaleTimeString();
-  timeElement.textContent = currentTime.toLocaleString();
-}, 1000);
